adcd2961ad1ffaae31fcc99c96ffdc0c0b960b4b
dario <darioleong1997@gmail.com>
2021-01-27 13:34:17 +0100

    Versión inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
 import { AlumnoComponent } from './pages/alumno/alumno.component';
+import { StudentCompanyViewComponent } from './pages/alumno/student-company-view/student-company-view.component';
 import { HomeComponent } from './pages/home/home.component';
 
 //const routes: Routes = [{ path: 'alumno', loadChildren: () => import('./pages/alumno/alumno.module').then(m => m.AlumnoModule) }];
@@ -13,7 +14,9 @@ const routes: Routes = [
   {path: 'admin/dashboard', component: AdminDashboardComponent},
 
   //Rutas alumno
-  {path: 'alumno/perfil', component: AlumnoComponent}
+  {path: 'alumno/perfil', component: AlumnoComponent},
+  {path: 'alumno/empresa-view', component: StudentCompanyViewComponent}
+
 ];
 
 @NgModule({
