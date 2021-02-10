e8b99a80409107a75ef53d49aa30a50f82fa2933
isra9shadow <isra9movil@hotmail.com>
2021-02-10 12:41:47 +0100

    Funciona docker

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "------npm install------"
-npm install
+npm install -y
 echo "-----FIN -----"
 echo "------Iniciar servidor------"
-npm start
+npm start -y
 
 echo "------Fin de Script------"
