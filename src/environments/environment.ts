7dd9aa121ca8d4e4ee16dc247d478c115b197430
isra9shadow <isra9movil@hotmail.com>
2021-02-19 05:37:29 +0100

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  Laravel: "http://coworkingdaw2.ddns.net:8082/api/",
+  Laravel: "https://coworkingdaw2.ddns.net:8082/api/",
   production: false
 };
 
