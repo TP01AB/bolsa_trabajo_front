440abd65ca753dd048ea2ca48512e66a4e91e83f
dario <darioleong1997@gmail.com>
2021-03-10 09:42:54 +0100

    fiexs varios

--- cypress/integration/registroAl.spec.js
+++ cypress/integration/registroAl.spec.js
@@ -6,6 +6,7 @@ describe('Probando registro de un alumno', () => {
         //cy.visit('http://localhost:4200/');
         cy.get('#registro').click();
         cy.url().should('include', '/registro')
+        cy.wait(5000);
         cy.get('#email').type('pepecom');
         cy.get('#password').type('1');
         cy.get('#opcion').select('Alumno');
@@ -53,6 +54,35 @@ describe('Probando registro de un alumno', () => {
 
         cy.url().should('include', '/login')
         cy.get('#email').type('pepe@gmail.com');
+        cy.get('#password').type('123');
+        cy.contains('Enviar').click();
+
+        cy.get('#message').should('have.text', 'Usuario desactivado,contacte con el administrador', '{ timeout: 20000 }')
+    })
+
+    it('Probando registro con campos correctos', () => {
+        //cy.visit('https://bolsatrabajovdg.netlify.app');
+        cy.visit('http://localhost:4200/');
+        cy.get('#registro').click();
+        cy.url().should('include', '/registro')
+        cy.get('#email').type('empresa@gmail.com');
+        cy.get('#password').type('123');
+        cy.get('#opcion').select('Empresa');
+
+        cy.get('#cif').type('S0982177H');
+        cy.get('#fecha').type('1990-4-5');
+        cy.get('#nombre').type('Ejemplo SA');
+        cy.get('#sector').type('Informatica');
+        cy.get('#descripcion').type('Esto es un texto descriptivo para una empresa que debe ocupar un minimo de caracteres');
+
+        cy.get('#btnRegist').click();
+        cy.wait(5000);
+        //cy.visit('https://bolsatrabajovdg.netlify.app');
+        cy.visit('http://localhost:4200/', '{ timeout: 20000 }');
+        cy.get('#login').click();
+
+        cy.url().should('include', '/login')
+        cy.get('#email').type('empresa@gmail.com');
         cy.get('#password').type('123');
         cy.contains('Enviar').click();
 
